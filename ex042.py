c56e29efb584268af56fbfb028cafaa52122bfaf
maroberto <maroberto13@gmail.com>
2021-02-18 20:57:26 -0300

    ex042 finalizado

--- ex042.py
+++ ex042.py
@@ -1,5 +1,6 @@
 class Triangulo:
-    def __init__(self, reta1, reta2, reta3):
+    def __init__(self,nome, reta1, reta2, reta3):
+        self.nome = nome
         self.reta1 = reta1
         self.reta2 = reta2
         self.reta3 = reta3
@@ -9,20 +10,21 @@ class Triangulo:
         n2 = self.reta2
         n3 = self.reta3
 
-        print(10 * '-=-')
+        print(17 * '-=-')
+        print('Olá {}'.format(self.nome))
+
         if (n1 + n2 > n3) and (n1 + n3 > n2) and (n3 + n2 > n1):
             print('É um triângulo')
             if (n1 == n2) and (n3 == n1) and (n3 == n2):
-                print('Equilátero')
-            elif (n1 + n3 > n2):
-                print('Isósceles')
-            elif (n3 + n2 > n1):
-                print('Escaleno')
+                print('Com todos os lados iguais este é um triângulo Equilátero!')
+            elif (n1 == n2) or (n3 == n1) or (n3 == n2):
+                print('Com dois lados iguais este é um triângulo isósceles!')
+            elif (n1 != n2) and (n3 != n1) and (n3 != n2):
+                print('Com todos os lados diferentes este é um triângulo escaleno!')
         else:
-            print('Não pode formar triângulo')
-        print(10 * '-=-')
+            print('Com essas medidas, não se pode formar um triângulo!')
+        print(17 * '-=-')
 
 
-tr1 = Triangulo(9, 10, 15)
+tr1 = Triangulo('Marcos', 11, 11, 175)
 tr1.Area()
-  
